73ba78e3fd2c7e986b34afcb302053bcc62404aa
AgungDwiW <agung.dwi.temp@gmail.com>
2019-12-11 23:27:30 +0700

    sendback image

--- batik_classification_web/controller.py
+++ batik_classification_web/controller.py
@@ -1,78 +1,14 @@
-from flask import Blueprint,render_template,flash,redirect
-from vendor import batik_classification as bc
-from flask import render_template
+from flask import Blueprint,render_template
 from flask import request
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras import applications
-from tensorflow.keras.models import Model,Sequential
-from tensorflow.keras.layers import Input, Activation, Dense, Conv2D, MaxPooling2D, ZeroPadding2D, Flatten
 import numpy as np
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import os
 from werkzeug import secure_filename
-from flask import Flask, redirect, url_for
 
 loaded_model = load_model("static/model/fixmodel.h5")
 batik_name = ['kawung.html', 'megaMendung.html', 'nitikKarawitan.html', 'parang.html', 'sidoLuhur.html',
               'tuntrum.html', 'udanLiris.html', 'kawung.html', 'ceplok.html', 'tambal.html', 'parang.html']
-"""
-bat_name = ['controller.bat0', 
-            'controller.bat1',
-            'controller.bat2',
-            'controller.bat3',
-            'controller.bat4',
-            'controller.bat5',
-            'controller.bat6',
-            'controller.bat7',
-            'controller.bat8',
-            'controller.bat9']
-"""
-
-#@WebApp.route('/'+batik_name[0][:-5], methods=['GET'])
-def bat0(filename):
-    return render_template(batik_name[0])
-#@WebApp.route('/'+batik_name[1][:-5], methods=['GET'])
-def bat1(filename):
-    return render_template(batik_name[1])
-#@WebApp.route('/'+batik_name[2][:-5], methods=['GET'])
-def bat2(filename):
-    return render_template(batik_name[2])
-#@WebApp.route('/'+batik_name[3][:-5], methods=['GET'])
-def bat3(filename):
-    return render_template(batik_name[3])
-#@WebApp.route('/'+batik_name[4][:-5], methods=['GET'])
-def bat4(filename):
-    return render_template(batik_name[4])
-#@WebApp.route('/'+batik_name[5][:-5], methods=['GET'])
-def bat5(filename):
-    return render_template(batik_name[5])
-#@WebApp.route('/'+batik_name[6][:-5], methods=['GET'])
-def bat6(filename):
-    return render_template(batik_name[6])
-#@WebApp.route('/'+batik_name[7][:-5], methods=['GET'])
-def bat7(filename):
-    return render_template(batik_name[7])
-#@WebApp.route('/'+batik_name[8][:-5], methods=['GET'])
-def bat8(filename):
-    return render_template(batik_name[8])
-#@WebApp.route('/'+batik_name[9][:-5], methods=['GET'])
-def bat9(filename):
-    return render_template(batik_name[9])
-
-
-
-bat_func =[bat0,
-         bat1,
-         bat2,
-         bat3,
-         bat4,
-         bat5,
-         bat6,
-         bat7,
-         bat8,
-         bat9,
-        ]
 
 def predict(img):
     img_width, img_height = 128, 128
